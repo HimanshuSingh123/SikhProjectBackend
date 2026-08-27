53fe97ff1596de6df495ec13a3504d0dfec63da2
Himanshu Singh <himanshusingh12@yahoo.com>
2026-08-27 18:48:32 -0400

    Favourites Controller completed

---
+++ Src.Application/Features/Favourite/Commands/AddToFavouritesCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Src.Application.Interfaces;
+
+namespace Src.Application.Features.Favourite.Commands;
+
+public class AddToFavouritesCommandHandler : IRequestHandler<AddToFavouritesCommand, bool>
+{
+    private readonly IFavouriteRepository _repository;
+    private readonly ILogger<AddToFavouritesCommandHandler> _logger;
+
+    public AddToFavouritesCommandHandler(ILogger<AddToFavouritesCommandHandler> logger, IFavouriteRepository repository)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public async Task<bool> Handle(AddToFavouritesCommand request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Adding submission {SubmissionId} to favourites for user {Username}", request.submissionId, request.Username);
+
+        var result = await _repository.AddToFavouritesAsync(request, cancellationToken);
+
+        if (result)
+        {
+            _logger.LogInformation("Successfully added submission {SubmissionId} to favourites for user {Username}", request.submissionId, request.Username);
+        }
+        else
+        {
+            _logger.LogWarning("Failed to add submission {SubmissionId} to favourites for user {Username}", request.submissionId, request.Username);
+        }
+
+        return result;
+    }
+}
--- Src.Application/Interfaces/IFavouriteRepository.cs
+++ Src.Application/Interfaces/IFavouriteRepository.cs
@@ -1,9 +1,16 @@
-﻿using Src.Domain.Favourite;
+﻿using Src.Domain.Entities;
+using Src.Domain.Favourite;
 
 namespace Src.Application.Interfaces;
 
 public interface IFavouriteRepository
 {
-    Task<IEnumerable<ViewFavouritesResponse>> GetFavouritesAsync(ViewFavouritesRequest request);
+    Task<IEnumerable<ViewFavouritesResponse>> GetFavouritesAsync(ViewFavouritesRequest request, CancellationToken cancellationToken);
+
+    Task<bool> AddToFavouritesAsync(AddToFavouritesRequest request, CancellationToken cancellationToken);
+
+    Task<Favourites?> SubmissionExtractor(AddToFavouritesRequest request, CancellationToken cancellationToken);
+
+    Task<bool> DeleteFromFavouritesAsync(DeleteFavouritesRequest request, CancellationToken cancellationToken);
 }
 
